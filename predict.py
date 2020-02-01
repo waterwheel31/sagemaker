c43a9f5bef3631b73d5e177dd7307472d9858099
waterwheel31 <waterwheel31@gmail.com>
2020-02-01 18:50:42 +0400

    updated

--- predict.py
+++ predict.py
@@ -87,6 +87,6 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = model(data)
+    result = np.round(model(data))
 
-    return result
+    return result.numpy()
